a38ae04819354d77a32e7de77a56ba5e2b6f2416
andreipatza <andreipatza@gmail.com>
2025-01-30 16:06:34 +0200

    UPdates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
+
+import type { Metadata } from "next";
 import "@/app/globals.css"
 import { Inter } from "next/font/google"
 
 const inter = Inter({ subsets: ["latin"] })
+
+
+export const metadata: Metadata = {
+  title: "One love - Andrei Pața",
+  description: "Fii eroul propriei tale povești.",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -12,4 +20,3 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   )
 }
-
