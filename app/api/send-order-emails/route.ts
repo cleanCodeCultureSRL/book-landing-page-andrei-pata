bef7948178a82b20351098ac9858fb6efb9df47d
andreipatza <andreipatza@gmail.com>
2025-01-31 14:54:44 +0200

    UPdates

--- app/api/send-order-emails/route.ts
+++ app/api/send-order-emails/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+import fs from "fs/promises";
+import path from "path";
 
 export async function POST(req: Request) {
   const testAccount = await nodemailer.createTestAccount();
